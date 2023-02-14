e6088cc9fa509b89adc395bc4c6d049797ad0930
jmtinycircuits <jmarcum@tinycircuits.com>
2023-02-14 15:54:57 -0500

    site

--- gulpfile-initial.js
+++
@@ -1,9 +0,0 @@
-var fileinclude = require('gulp-file-include');
-var gulp = require('gulp');
- 
-gulp.src(['./src/html/**/*.html'])
-.pipe(fileinclude({
-    prefix: '@@',
-    basepath: '@file'
-}))
-.pipe(gulp.dest('./dist'));
--- src/js/choose.js
+++
@@ -1,9 +0,0 @@
-const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-});
-
-let value = params.tvtype;
-
-if(value == "diy"){
-    document.getElementById("stream").parentElement.removeChild(document.getElementById("stream"));
-}
