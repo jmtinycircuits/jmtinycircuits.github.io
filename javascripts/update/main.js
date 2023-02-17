6a1f591ded1af306ee4c04eb1fb3e504b8371878
jmtinycircuits <jmarcum@tinycircuits.com>
2023-02-17 16:08:41 -0500

    diy test

--- javascripts/update/main.js
+++ javascripts/update/main.js
@@ -1,5 +1,11 @@
 import { Serial } from "../serial.js";
 import { TV_TYPES } from "/javascripts/streaming/jpegstreamerCommon.js";
+import { BasicPicoboot } from "./basicpicoboot.js";
+
+
+
+
+
 
 
 // Only perform the streaming logic if actually on the streaming page that has all the elements
@@ -60,14 +66,14 @@ if(window.location.pathname.indexOf("Update") != -1){
     }
 
 
-    // When insertUrlParam(), removeUrlParameter(), or page back button are pressed, re-render the page
+    // When insertUrlParameter(), removeUrlParameter(), or page back button are pressed, re-render the page
     window.onpopstate = () => {
         refresh();
     }
     
     
     // https://stackoverflow.com/questions/10970078/modifying-a-query-string-without-reloading-the-page
-    let insertUrlParam = (key, value) => {
+    let insertUrlParameter = (key, value) => {
         if (history.pushState) {
             let searchParams = new URLSearchParams(window.location.search);
             searchParams.set(key, value);
@@ -77,219 +83,179 @@ if(window.location.pathname.indexOf("Update") != -1){
         }
     }
     
-    // to remove the specific key
-    let removeUrlParameter = (paramKey) => {
+    // Remove the specific key
+    let removeUrlParameter = (key) => {
         const url = window.location.href;
-        console.log("url", url);
         var r = new URL(url);
-        r.searchParams.delete(paramKey);
+        r.searchParams.delete(key);
         const newUrl = r.href;
-        console.log("r.href", newUrl);
         window.history.pushState({ path: newUrl }, '', newUrl);
         refresh();
     }
 
+    // Get value of specific key
+    let getUrlParameter = (key) => {
+        let searchParams = new URLSearchParams(window.location.search);
+        return searchParams.get(key);
+    }
 
 
-    // Makes all elements invisible and then shows element for screen in query string (called by insertUrlParam)
+    if(!("serial" in navigator)){
+        disable("connectButton");
+        show("browserSupportError");
+        hide("description");
+    }
+
+    let serial = new Serial([{usbVendorId:11914, usbProductId:10}, {usbVendorId:0x03EB, usbProductId: 0x8008}, {usbVendorId:0x03EB, usbProductId: 0x8009}], false);
+
+
+
+    async function upload(firmwareName){
+        // chrome://device-log/?refresh=1
+        const decoder = new TextDecoder();
+
+        let collectedData = "";
+        serial.onData = (data) => {
+            collectedData += decoder.decode(data);
+            console.log(decoder.decode(data));
+        }
+
+        let checkCollectedDataFor = async (text) => {
+            return new Promise((resolve, reject) => {
+                let check = () => {
+                    setTimeout(() => {
+                        if(collectedData.indexOf(text) == -1){
+                            check();
+                            console.warn("looking for " + text);
+                        }else{
+                            collectedData = "";
+                            resolve(true);
+                        }
+                    }, 10);
+                }
+                check();
+            });
+        }
+
+        let wait = async (ms) => {
+            return new Promise((resolve, reject) => {
+                let count = 0;
+                let wait1ms = () => {
+                    setTimeout(() => {
+                        if(count < ms){
+                            count++;
+                            wait1ms();
+                        }else{
+                            resolve();
+                        }
+                    }, 1);
+                }
+                wait1ms();
+            });
+        }
+
+        const programFlashStart = 0x2000;
+        const uploadPacketSize = 4096;
+        const sramBufferAddress = 0x20005000;   // Taken from Arduino IDE output with verbose option enabled
+        
+
+        const binData = new Uint8Array(await (await fetch(firmwareName)).arrayBuffer());
+        const packetCount = Math.ceil(binData.byteLength/uploadPacketSize);
+
+        serial.onDisconnect = () => {
+            removeUrlParameter("screen");
+        }
+        serial.onConnect = async () => {
+            console.warn("...");
+
+            show("progressBar");
+            document.getElementById("progressBarBar").style.width = "0%";
+
+            // Erase flash after bootloader
+            await serial.write("X" + programFlashStart.toString(16) + "#", true);
+            await wait(1);
+            await checkCollectedDataFor("X\n\r");
+
+            for(let ipx=0; ipx<packetCount; ipx++){
+                let packet = binData.slice((ipx*uploadPacketSize), (ipx*uploadPacketSize)+uploadPacketSize)
+
+                // https://github.com/shumatech/BOSSA/blob/master/src/Samba.cpp#L511
+                const cmd0 = "S" + sramBufferAddress.toString(16) + "," + packet.byteLength.toString(16).padStart(4, '0') + "#";
+                console.log(cmd0);
+                await serial.write(cmd0, true);
+                await wait(1);
+
+                // https://github.com/shumatech/BOSSA/blob/master/src/Samba.cpp#L528
+                await serial.write(packet, false);
+                await wait(1);
+
+                // https://github.com/shumatech/BOSSA/blob/master/src/Samba.cpp#L619
+                const cmd1 = "Y" + sramBufferAddress.toString(16) + ",0#";
+                console.log(cmd1);
+                await serial.write(cmd1, true);
+                await wait(1);
+                await checkCollectedDataFor("Y\n\r");
+
+                // https://github.com/shumatech/BOSSA/blob/master/src/Samba.cpp#L629
+                const cmd2 = "Y" + (programFlashStart + (ipx*uploadPacketSize)).toString(16) + "," + packet.byteLength.toString(16).padStart(4, '0') + "#";
+                console.log(cmd2);
+                await serial.write(cmd2, true);
+                await wait(1);
+                await checkCollectedDataFor("Y\n\r");
+                await wait(1);
+
+                console.log("");
+
+                document.getElementById("progressBarBar").style.width = ((ipx/packetCount) * 100) + "%";
+            }
+
+            // CPU reset https://github.com/shumatech/BOSSA/blob/3532de82efd28fadbabc2b258d84dddf14298107/src/Device.cpp#L652
+            await serial.write("WE000ED0C,05FA0004#", true);
+        }
+
+        await serial.connect();
+    }
+
+
+
+    // Makes all elements invisible and then shows element for screen in query string (called by insertUrlParameter)
     let refresh = () => {
-        const screen = window.location.search.substring(window.location.search.indexOf("=")+1);
+        const screen = getUrlParameter("screen");
 
         // Only hide all elements if in a screen other than main update landing screen
         hideAll();
 
-        if(screen == ""){
-            setInnerText("description", "Update software on TinyTV 2, Mini, or DIY Kit");
+        if(screen == undefined){
+            setClickCallback("connectButton", serial.connect.bind(serial, 1200, 128));
+
+            serial.onConnect = () => {
+                serial.onDisconnect = () => {
+                    insertUrlParameter("screen", "diy_update");
+                }
+                serial.disconnect();
+            }
+
+            setInnerText("description", "Update software on TinyTV 2, Mini, or DIY Kit.\nConnect TV to put into update mode.");
             setInnerText("connectButton", "Connect TV");
             show("description");
             show("connectButton");
-        }else if(screen == "manual_update"){
-            setInnerText("infoOutput", "Manually choose your TV");
-            show("manualChoice");
-            show("infoOutput");
-            show("cancelManualUpdate");
 
-            setClickCallback("choseMini", () => {
-                insertUrlParam("screen", "tvmini_step_1");
-            });
-            setClickCallback("chose2", () => {
-                insertUrlParam("screen", "tv2_step_1");
-            });
-            setClickCallback("choseDIY", () => {
-                insertUrlParam("screen", "tvdiy_step_1");
-            });
-            setClickCallback("cancelManualUpdate", () => {
-                removeUrlParameter("screen");
-            });
-        }else if(screen == "tvmini_step_1"){
-
-        }else if(screen == "tv2_step_1"){
-            setInnerText("description", "Step 1: Turn the TV off by holding the power button for 5 seconds. The screen will remain off\n(it does not matter if the TV is already on or off)");
+            // If serial connected, disconnect it on this page
+            if(serial.connected){
+                serial.disconnect();
+            }
+        }else if(screen == "diy_update"){
+            setInnerText("description", "Ready to update\nChoose the TV again to update");
+            setInnerText("connectButton", "Connect To Update");
             show("description");
-            show("backButton");
-            show("nextButton");
-            show("tv2Step1VideoContainer");
-            play("tv2Step1Video");
-            show("cancelManualUpdate");
+            show("connectButton");
 
-            setClickCallback("nextButton", () => {
-                insertUrlParam("screen", "tv2_step_2");
-            });
-            setClickCallback("backButton", () => {
-                insertUrlParam("screen", "manual_update");
-            });
-            setClickCallback("cancelManualUpdate", () => {
-                removeUrlParameter("screen");
-            });
-        }else if(screen == "tv2_step_2"){
-            setInnerText("description", "Step 2: Plug the TV into a computer using a USB-C cord");
-            show("description");
-            show("backButton");
-            show("nextButton");
-            show("tv2Step2VideoContainer");
-            play("tv2Step2Video");
-            show("cancelManualUpdate");
-
-            setClickCallback("nextButton", () => {
-                insertUrlParam("screen", "tv2_step_3");
-            });
-            setClickCallback("backButton", () => {
-                insertUrlParam("screen", "tv2_step_1");
-            });
-            setClickCallback("cancelManualUpdate", () => {
-                removeUrlParameter("screen");
-            });
-        }else if(screen == "tv2_step_3"){
-            setInnerText("description", "Step 3: Turn the TV on in update mode by pressing and holding\nthe reset button with a pen and clicking the power button once");
-            show("description");
-            show("backButton");
-            show("nextButton");
-            show("tv2Step3VideoContainer");
-            play("tv2Step3Video");
-            show("cancelManualUpdate");
-
-            setClickCallback("nextButton", () => {
-                insertUrlParam("screen", "update");
-            });
-            setClickCallback("backButton", () => {
-                insertUrlParam("screen", "tv2_step_2");
-            });
-            setClickCallback("cancelManualUpdate", () => {
-                removeUrlParameter("screen");
-            });
-        }else if(screen == "tvdiy_step_1"){
-            
-        }else if(screen == "update"){
-            show("cancelManualUpdate");
-            setClickCallback("cancelManualUpdate", () => {
-                removeUrlParameter("screen");
+            setClickCallback("connectButton", () => {
+                upload("/firmware/d-0.bin");
             });
         }
     }
 
     // Call this in case link with query string is visited
     refresh();
-
-
-    // If user picks manual update, change query string and render that page
-    setClickCallback("manualUpdateButton", () => {
-        insertUrlParam("screen", "manual_update");
-    })
-
-
-    if (!("serial" in navigator)){
-        disable("connectButton");
-        show("browserSupportError");
-        hide("description");
-    }
-
-    let serial = new Serial([{usbVendorId:11914, usbProductId:10}]);
-    let detectedTVType = TV_TYPES.NONE;
-
-
-    let onTVTypeDetected = () => {
-        show("infoOutput");
-        if(detectedTVType == TV_TYPES.TINYTV_2){
-            console.error("TINY TV 2");
-        }else if(detectedTVType == TV_TYPES.TINYTV_MINI){
-            console.error("TINY TV MINI");
-        }else if(detectedTVType == TV_TYPES.TINYTV_DIY){
-            console.error("TINY TV DIY");
-        }
-    }
-
-    let failedToConnectOrDetect = (str) => {
-        detectedTVType = TV_TYPES.NONE
-        serial.disconnect(false);
-
-        setClickCallback("connectButton", serial.connect.bind(serial));
-        setInnerText("description", str);
-        setInnerText("connectButton", "Try Again");
-
-        hide("infoOutput");
-        show("description");
-        show("manualUpdateButton");
-    }
-
-
-    serial.onConnect = () => {
-        setClickCallback("connectButton", serial.disconnect.bind(serial));
-        setInnerText("connectButton", "Disconnect");
-
-        show("infoOutput");
-        hide("description");
-        hide("manualUpdateButton");
-
-        const decoder = new TextDecoder();
-        let received = "";
-
-        serial.onData = (data) => {
-            if(detectedTVType == TV_TYPES.NONE){
-                received += decoder.decode(data);
-
-                // See if it is any of the TVs, pass a human readable string to the on detect function since it will be displayed
-                if(received.indexOf(TV_TYPES.TINYTV_2) != -1){
-                    detectedTVType = TV_TYPES.TINYTV_2;
-                    onTVTypeDetected();
-                }else if(received.indexOf(TV_TYPES.TINYTV_MINI) != -1){
-                    detectedTVType = TV_TYPES.TINYTV_MINI;
-                    onTVTypeDetected();
-                }
-            }
-        }
-
-        let attempts = 0; 
-        let requestTVType = () => {
-            if(detectedTVType == TV_TYPES.NONE && serial.connected){
-                setTimeout(() => {
-                    serial.write("TYPE", true);
-
-                    attempts++;
-
-                    // 5 seconds
-                    if(attempts >= 20){
-                        failedToConnectOrDetect("Detection failed. Would you like to try again or try manually updating?");
-                    }else{
-                        requestTVType();
-                    }
-                }, 250);
-            }
-        }
-        requestTVType();
-    }
-    serial.onConnectionCanceled = () => {
-        failedToConnectOrDetect("Connection canceled. Would you like to try again or try manually updating?");
-    }
-    serial.onDisconnect = () => {
-        setClickCallback("connectButton", serial.connect.bind(serial));
-        setInnerText("description", "Update software on TinyTV 2, Mini, or DIY Kit");
-        setInnerText("connectButton", "Connect TV");
-
-        hide("infoOutput")
-        show("description");
-
-        detectedTVType = TV_TYPES.NONE;
-    }
-
-    setClickCallback("connectButton", serial.connect.bind(serial));
 }
